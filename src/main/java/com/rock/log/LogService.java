230b67309a2c3271e8adac07e79322657f10fade
rock117 <manj22@163.com>
2014-03-17 23:17:44 +0800

    add all

--- src/main/java/com/rock/log/LogService.java
+++ src/main/java/com/rock/log/LogService.java
@@ -1,15 +1,25 @@
 package com.rock.log;
 
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
-
+import org.aspectj.lang.Signature;
 @Component
 public class LogService{
 
 	public void logAround(ProceedingJoinPoint joinPoint) throws Throwable{
+		printInvokeInfo(joinPoint);
 		System.out.println("logging around before");
 		joinPoint.proceed();
 		System.out.println("logging around after");
 	}
 
+	private void printInvokeInfo(ProceedingJoinPoint joinPoint){
+		Object target = joinPoint.getTarget();
+		String classname = target.getClass().getName();
+	    Signature sinature =	joinPoint.getSignature();
+	    String methodName = sinature.getName();
+	    System.out.println("invoke "+classname+"."+methodName);
+	}
+	
 }
