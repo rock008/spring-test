4e66c6d835105a520aba39ab8d6b2a0bf0d06080
rock117 <manj22@163.com>
2014-03-17 23:06:00 +0800

    add all

--- src/main/java/com/rock/log/LogService.java
+++ src/main/java/com/rock/log/LogService.java
@@ -1,19 +1,15 @@
 package com.rock.log;
 
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LogService implements MethodInterceptor{
+public class LogService{
 
-	public void logAround(){
-		System.out.println("logging around");
-	}
-
-	@Override
-	public Object invoke(MethodInvocation arg0) throws Throwable {
-		return arg0.proceed();
+	public void logAround(ProceedingJoinPoint joinPoint) throws Throwable{
+		System.out.println("logging around before");
+		joinPoint.proceed();
+		System.out.println("logging around after");
 	}
 
 }
